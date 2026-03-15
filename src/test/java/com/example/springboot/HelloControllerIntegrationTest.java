fdb73176ba8e164110d1753aaa5b28eb9376bb5f
zazadior <zazadior@LunarSailor.myguest.virtualbox.org>
2026-03-15 10:23:35 -0400

    Initial Spring Boot project

---
+++ src/test/java/com/example/springboot/HelloControllerIntegrationTest.java
@@ -0,0 +1,23 @@
+package com.example.springboot;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureRestTestClient
+public class HelloControllerIntegrationTest {
+
+  @Autowired
+  private RestTestClient client;
+
+  @Test
+  public void getHello() {
+    client.get().uri("/").exchangeSuccessfully()
+        .expectBody(String.class)
+        .isEqualTo("Greetings from Spring Boot!");
+  }
+}
